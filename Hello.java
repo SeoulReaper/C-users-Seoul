42c488b7913bd635bd6a3e5983c057fa368fafae
SeoulReaper <69983460+SeoulReaper@users.noreply.github.com>
2020-08-20 13:56:10 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+public static void main(String[]args){
+	System.out.println("<Seoul. Kei Hudson>");
+	System.out.println("<Hello java>");
+	System.out.println("<First lab>");
+	System.out.println("<20 Aug 2020>");
+	System.out.println("<CRN 11726 sect. 001>");
+	System.out.println();
+	}	
+}
